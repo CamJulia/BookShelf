f37a4dc520a476d67a08ff1e8e348146d8ff03b4
Julia Çam <barbarbetsy@googlemail.com>
2019-05-28 20:46:34 +0200

    feat: add search function for search terms

--- src/components/BooksAll.jsx
+++ src/components/BooksAll.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import BookSection from './BookSection';
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 class BooksAll extends React.Component {
 
--- src/components/SearchTerms.jsx
+++ src/components/SearchTerms.jsx
@@ -1,27 +1,110 @@
 // Shows search term.
-import React from 'react';
+import React from "react";
 // import { search } from '../BooksAPI.js';
-import * as BooksAPI from '../BooksAPI';
+import * as BooksAPI from "../BooksAPI";
 
-const searchTerms = ['Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS']
+const searchTerms = [
+  "Android",
+  "Art",
+  "Artificial Intelligence",
+  "Astronomy",
+  "Austen",
+  "Baseball",
+  "Basketball",
+  "Bhagat",
+  "Biography",
+  "Brief",
+  "Business",
+  "Camus",
+  "Cervantes",
+  "Christie",
+  "Classics",
+  "Comics",
+  "Cook",
+  "Cricket",
+  "Cycling",
+  "Desai",
+  "Design",
+  "Development",
+  "Digital Marketing",
+  "Drama",
+  "Drawing",
+  "Dumas",
+  "Education",
+  "Everything",
+  "Fantasy",
+  "Film",
+  "Finance",
+  "First",
+  "Fitness",
+  "Football",
+  "Future",
+  "Games",
+  "Gandhi",
+  "Homer",
+  "Horror",
+  "Hugo",
+  "Ibsen",
+  "Journey",
+  "Kafka",
+  "King",
+  "Lahiri",
+  "Larsson",
+  "Learn",
+  "Literary Fiction",
+  "Make",
+  "Manage",
+  "Marquez",
+  "Money",
+  "Mystery",
+  "Negotiate",
+  "Painting",
+  "Philosophy",
+  "Photography",
+  "Poetry",
+  "Production",
+  "Programming",
+  "React",
+  "Redux",
+  "River",
+  "Robotics",
+  "Rowling",
+  "Satire",
+  "Science Fiction",
+  "Shakespeare",
+  "Singh",
+  "Swimming",
+  "Tale",
+  "Thrun",
+  "Time",
+  "Tolstoy",
+  "Travel",
+  "Ultimate",
+  "Virtual Reality",
+  "Web Development",
+  "iOS"
+];
 
-let booksFound = (term) => {
-  BooksAPI.search(term)
-  return BooksAPI.search.data;
-};
+const handleClick = () => {
+  console.log('this is:', this);
+}
 
+const terms = searchTerms.map(term => (
+  <button key={term} className="search-term" onClick={handleClick}>
+    {term}
+  </button>
+));
 
-const terms = searchTerms.map((term) =>
-  <button key={term} className='search-term' onClick={booksFound(term)}>{term}</button>
-);
+//  const booksFound = () => {
+//   // await BooksAPI.search(term);
+//   console.log('Hello')
+//   const books = BooksAPI.search(this);
+//   console.log (books);
+// };
 
 class SearchTerms extends React.Component {
   render() {
-    return (
-      <div className='search-terms'>
-        {terms}
-      </div>
-    )
+    return <div className="search-terms">{terms}</div>;
   }
 }
 
