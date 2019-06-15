1c943d8b9af6eaa163faaf90acae9aff1889a946
Julia Çam <julia.cam@evolu.software>
2019-06-15 21:48:41 +0200

    style: clean up code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.render(
-<BrowserRouter>
-<App />
-</BrowserRouter>
-, document.getElementById('root'))
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    , document.getElementById('root'))
